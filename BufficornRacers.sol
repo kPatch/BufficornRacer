738df4c192504a4d9f5a87bf09acc944c45f5b62
Antoine De Vuyst <fortyonepointsix@gmail.com>
2023-03-05 15:24:12 -0300

    Update BufficornRacers.sol

--- BufficornRacers.sol
+++ BufficornRacers.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MyToken is ERC721 {
+
+contract BufficornRacers is ERC721 {
     constructor() ERC721("Bufficorn Racers", "BUFFRACER") {}
 
     uint public counter;
 
+    string[] metadata;
+
+    string public MetadataURL = "https://raw.githubusercontent.com/kPatch/BufficornRacer/main/Metadata/Metadata/";
+
     function mint(address _address) public {
+        metadata[counter] = string.concat(string.concat(MetadataURL,Strings.toString(counter)),".json");
         _safeMint(_address, counter++);
     }
 }
