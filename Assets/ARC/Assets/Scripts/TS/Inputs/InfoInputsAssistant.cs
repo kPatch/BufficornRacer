787991a359f7f5051e7fae37c4d1079409bf1eb0
Xiangxu Lin <cricel.coder@gmail.com>
2023-03-05 08:20:19 -0700

    Init repo (parts)

---
+++ Assets/ARC/Assets/Scripts/TS/Inputs/InfoInputsAssistant.cs
@@ -0,0 +1,25 @@
+﻿// Description: InfoInputsAssistant. Useful method to use in association with InfoInputs
+using UnityEngine;
+
+namespace TS.Generics
+{
+    public class InfoInputsAssistant : MonoBehaviour
+    {
+        public float MobileInputExample()
+        {
+            if (ControlFreak2.CF2Input.GetKey(KeyCode.E))
+                return 1;
+            else
+                return 0;
+        }
+
+        public float OtherInputExample()
+        {
+            if (ControlFreak2.CF2Input.GetKey(KeyCode.F))
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+}
---
+++ Assets/ARC/Assets/Scripts/TS/UI/Championship/PressAnyKey.cs
@@ -0,0 +1,32 @@
+﻿// Description: PressAnyKey: Wait until the player press a key to load the nex track in championship mode
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TS.Generics
+{
+    public class PressAnyKey : MonoBehaviour
+    {
+        private bool bOnce;
+
+        public UnityEvent loadNextTrack;
+
+        void Update()
+        {
+            if (ControlFreak2.CF2Input.anyKeyDown && !bOnce && !InfoPlayerTS.instance.b_IsPageCustomPartInProcess)
+            {
+                bOnce = true;
+                loadNextTrack.Invoke();
+            }
+        }
+
+        //-> Load New Scene
+        public void LoadSceneUsingCurrentSelectedTrackName()
+        {
+            string trackName = GameModeGlobal.instance.currentSelectedTrack;
+            LoadScene.instance.LoadSceneWithSceneNameAndSpecificCustomMethodList(trackName);
+        }
+
+
+    }
+
+}
